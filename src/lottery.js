38b3cadf41b0b4486c040b14a8ea7f02cfe6e2ab
Thomas Payne <thpa9933@colorado.edu>
2021-10-29 13:21:23 -0700

    lottery dapp

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+// CREATES LOCAL COPY ON CONRACT TO USE
+import web3 from "./web3";
+
+const address = "0x2C7656dFC3efB13A5B4f5fb63Ec63a16A4b88Bae";
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
